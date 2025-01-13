903aa342de3a464fab63af747b608c47ff449213
Woong Jung <kaka366@naver.com>
2025-01-13 23:35:01 +0900

    문제09 10진수 ->2진수 변환

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,29 @@
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Stack;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new int[]{4, 2, 2, 1, 3, 4})));
-        System.out.println(Arrays.toString(solution(new int[]{2, 1, 1, 3, 2, 5, 4})));
+    public static String solution(int decimal){
+        Stack<Integer> stack = new Stack<>();
+        while(decimal >0){
+            int remainder = decimal %2;
+            stack.push(remainder);
+            decimal /=2;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.toString();
     }
 
-    // 이 부분을 변경해서 실행해보세요.
-    private static int[] solution(int[] arr) {
-        Integer[] result = Arrays.stream(arr).boxed().distinct().toArray(Integer[]::new); // ❶ 중복값 제거
-        Arrays.sort(result, Collections.reverseOrder());                                  // ❷ 내림차순 정렬
-        return Arrays.stream(result).mapToInt(Integer::intValue).toArray();  // int형 배열로 변경 후 반환
+    public static void main(String[] args) {
+        String s = solution(13);
+        System.out.println(s);
+
     }
+
+
 
 }
