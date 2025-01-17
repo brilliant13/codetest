0296ebbfccf2d1db328121c1211e40f69bf25997
Woong Jung <kaka366@naver.com>
2025-01-17 12:00:15 +0900

    Union & Find Algorithm

--- src/Solution.java
+++ src/Solution.java
@@ -1,50 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class Solution {
-    private static String[] solution(int[] nodes) {
-        String[] result = new String[3];
-        result[0] = preorder(nodes, 0).trim();
-        result[1] = inorder(nodes, 0).trim();
-        result[2] = postorder(nodes, 0).trim();
-        return result;
+    //부모 저장을 위한 배열
+    private static int[] parent;
+
+    private static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        parent[x] = find(parent[x]);
+        return parent[x];
     }
 
-    private static String preorder(int[] nodes, int idx) {
-        if (idx >= nodes.length) {
-            return "";
-        }
-        //루트->왼쪽서브트리->오른쪽서브트리 순으로 재귀호출하여 결과를 이어붙인다.
-        return nodes[idx] + " " +
-                preorder(nodes, 2 * idx + 1) +
-                preorder(nodes, 2 * idx + 2);
+    private static void union(int x, int y) {
+        int root1 = find(x);
+        int root2 = find(y);
+        parent[root2] = root1;
     }
 
-    private static String inorder(int[] nodes, int idx) {
-        if (idx >= nodes.length) {
-            return "";
+    private static Boolean[] solution(int k, int[][] operation) {
+        parent = new int[k + 1];
+        for (int i = 1; i < k + 1; i++) {
+            parent[i] = i;
         }
-        //왼쪽 서브트리 -> 루트 노드 -> 오른쪽 서브트리 순을 재귀 호출하여 결과를 이어 붙임
-        return inorder(nodes, 2 * idx + 1) +
-                nodes[idx] + " " +
-                inorder(nodes, 2 * idx + 2);
-    }
+        ArrayList<Boolean> answer = new ArrayList<>();
+        for (int[] op : operation) {
+            if (op[0] == 0) {
+                union(op[1], op[2]);
+            } else {
+                answer.add(find(op[1]) == find(op[2]));
+            }
+        }
+        return answer.toArray(new Boolean[0]);
 
-    private static String postorder(int[] nodes, int idx) {
-        if (idx >= nodes.length) {
-            return "";
-        }
-        //왼쪽 서브 트리 -> 오른쪽 서브 트리 -> 루트 노드 순으로 재귀 호출하여 결과를 이어 붙임
-        return postorder(nodes, 2 * idx + 1) +
-                postorder(nodes,2*idx+2)+
-                nodes[idx] + " ";
     }
 
     public static void main(String[] args) {
-        int nodes[] = {1, 2, 3, 4, 5, 6, 7};
-        System.out.println(solution(nodes)[0]);
-        System.out.println(solution(nodes)[1]);
-        System.out.println(solution(nodes)[2]);
+        int[][] operations1 = {{0, 0, 1}, {0, 1, 2}, {1, 1, 2}};
+        System.out.println(Arrays.toString(solution(3, operations1)));
+        int[][] operations2 = {{0, 0, 1}, {1, 1, 2}, {0, 1, 2}, {1, 0, 2}};
+        System.out.println(Arrays.toString(solution(3, operations2)));
+
     }
 
 
