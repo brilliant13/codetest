1795231c0502a84af92efddfbee18c6e1f1e3cd2
Woong Jung <kaka366@naver.com>
2025-02-06 14:02:14 +0900

    문제69 조약돌 문제

--- src/Solution.java
+++ src/Solution.java
@@ -1,50 +1,47 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-
 public class Solution {
-    //부모 저장을 위한 배열
-    private static int[] parent;
-
-    private static int find(int x) {
-        if (parent[x] == x) {
-            return x;
-        }
-        parent[x] = find(parent[x]);
-        return parent[x];
-    }
-
-    private static void union(int x, int y) {
-        int root1 = find(x);
-        int root2 = find(y);
-        parent[root2] = root1;
-    }
-
-    private static Boolean[] solution(int k, int[][] operation) {
-        parent = new int[k + 1];
-        for (int i = 1; i < k + 1; i++) {
-            parent[i] = i;
-        }
-        ArrayList<Boolean> answer = new ArrayList<>();
-        for (int[] op : operation) {
-            if (op[0] == 0) {
-                union(op[1], op[2]);
-            } else {
-                answer.add(find(op[1]) == find(op[2]));
-            }
-        }
-        return answer.toArray(new Boolean[0]);
-
-    }
 
     public static void main(String[] args) {
-        int[][] operations1 = {{0, 0, 1}, {0, 1, 2}, {1, 1, 2}};
-        System.out.println(Arrays.toString(solution(3, operations1)));
-        int[][] operations2 = {{0, 0, 1}, {1, 1, 2}, {0, 1, 2}, {1, 0, 2}};
-        System.out.println(Arrays.toString(solution(3, operations2)));
-
+        int[][] arr1 = {{1, 3, 3, 2}, {2, 1, 4, 1}, {1, 5, 2, 3}};
+        System.out.println(solution(arr1));
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[0].length; j++) {
+                System.out.print(arr1[i][j]+" ");
+            }
+            System.out.println();
+        }
+        int[][] arr2 = {{1, 7, 13, 2, 6}, {2, -4, 2, 5, 4}, {5, 3, 5, -3, 1}};
+        System.out.println(solution(arr2));
     }
 
+    // 이 부분을 변경해서 실행해보세요.
+    private static int solution(int[][] arr) {
+        // ❶ 입력 배열의 열의 개수를 저장합니다.
+        int n = arr[0].length;
+        // ❷ dp 배열을 초기화합니다. 4행 n열의 2차원 배열입니다.
+        int[][] dp = new int[4][n];
+
+        // 각 열에서 선택 가능한 4가지 조약돌 배치 패턴에 대해 첫 번째 열의 가중치를 초기화합니다.
+        // ❸ 0: 상단, 1: 중앙, 2: 하단, 3: 상단과 하단
+        dp[0][0] = arr[0][0];
+        dp[1][0] = arr[1][0];
+        dp[2][0] = arr[2][0];
+        dp[3][0] = arr[0][0] + arr[2][0];
+
+        // ❹ 두 번째 열부터 마지막 열까지 각 열에서 선택 가능한 4가지 조약돌 배치 패턴에 대해
+        // 최대 가중치를 계산합니다.
+        for (int i = 1; i < n; i++) {
+            // 패턴 0이 선택된 경우, 이전은 패턴 {1, 2} 가능
+            dp[0][i] = arr[0][i] + Math.max(dp[1][i - 1], dp[2][i - 1]);
+            // 패턴 1이 선택된 경우, 이전은 패턴 {0, 2, 3} 가능
+            dp[1][i] = arr[1][i] + Math.max(dp[0][i - 1], Math.max(dp[2][i - 1], dp[3][i - 1]));
+            // 패턴 2이 선택된 경우, 이전은 패턴 {0, 1} 가능
+            dp[2][i] = arr[2][i] + Math.max(dp[0][i - 1], dp[1][i - 1]);
+            // 패턴 3이 선택된 경우, 이전은 패턴 {1} 가능
+            dp[3][i] = arr[0][i] + arr[2][i] + dp[1][i - 1];
+        }
+
+        // ❺ 마지막 열에서 선택 가능한 4가지 조약돌 배치 패턴 중 최대 가중치를 반환합니다.
+        return Math.max(Math.max(dp[0][n - 1], dp[1][n - 1]), Math.max(dp[2][n - 1], dp[3][n - 1]));
+    }
 
 }
