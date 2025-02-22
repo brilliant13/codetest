60e93d33826079c772d6f0650418476cb35ff111
Woong Jung <kaka366@naver.com>
2025-02-23 02:51:00 +0900

    ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,27 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //Test Case 개수
-        int N = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        //반복 횟수(R), 문자열(S) 입력
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int R = Integer.parseInt(st.nextToken());
-            String S = st.nextToken();
-            for (int j = 0; j < S.length(); j++) {
-                for (int k = 0; k < R; k++) {
-                    sb.append(S.charAt(j));
-                }
-            }
-            sb.append('\n');
+        Scanner in = new Scanner(System.in);
+        String arr[] = new String[in.nextInt()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.next();
         }
-        System.out.println(sb);
+        in.close();
 
+        for (int i = 0; i < arr.length; i++) {
+            int cnt = 0;
+            int sum = 0;
+            for (int j = 0; j < arr[i].length(); j++) {
+                if (arr[i].charAt(j) == 'O') {
+                    cnt++;
+                } else {
+                    cnt = 0;
+                }
+                sum += cnt;
+            }
+            System.out.println(sum);
+        }
     }
 }
