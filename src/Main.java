0f8a86a5e7d378a3f7c988f4b20a8568f149eae0
Woong Jung <kaka366@naver.com>
2025-05-01 18:21:01 +0900

    스택수열

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,49 @@
 import java.io.*;
-import java.util.*;
+import java.util.Stack;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // 수열 원소 개수 입력
+        int n = Integer.parseInt(br.readLine());
+        // 1 <=   <= n 원소 입력
+        int max = 1; //뽑힌 수열의 최댓값 +1
+        int target =0;
+        Stack<Integer> stack = new Stack<>(); //스택 자료구조
+        StringBuilder sb = new StringBuilder();
+        boolean isPossible = true; // 수열 생성 가능 여부
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int arr[] = new int[N];
-        int M = Integer.parseInt(st.nextToken());
-        int temp;
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i]=i+1;
+        for (int i = 0; i < n; i++) {
+            target = Integer.parseInt(br.readLine());
+            if (target >= max) {
+                //타겟이 현재 max보다 큰 경우
+                for (int j = max; j <= target; j++) {
+                    stack.push(j);
+                    sb.append("+\n");
+                }
+                max = target+1;
+                stack.pop();
+                sb.append("-\n");
+            } else {
+                //타겟이 현재 max보다 작은 경우
+                if (!stack.isEmpty() && target == stack.peek()) {
+                    stack.pop();
+                    sb.append("-\n");
+                } else {
+                    isPossible = false;
+                    break;
+                }
+            }
+
         }
-        for (int j = 0; j < M; j++) {
-            st = new StringTokenizer(br.readLine());
-
-            int I = Integer.parseInt(st.nextToken());
-            int J = Integer.parseInt(st.nextToken());
-
-            temp = arr[I - 1];
-            arr[I - 1] = arr[J - 1];
-            arr[J-1]=temp;
+        // 수열 생성 가능 여부에 따른 출력
+        if (isPossible) {
+            System.out.println(sb);
+        } else {
+            System.out.println("NO");
         }
-        for (int k = 0; k < arr.length; k++) {
-            bw.write(arr[k]+" ");
-        }
-        br.close();
-        bw.flush();
-        bw.close();
     }
 }
+
