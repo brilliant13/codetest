37b550035ac60cfad9dcfb3b98c52bfb70b20587
Woong Jung <kaka366@naver.com>
2025-09-08 01:41:50 +0900

    Divide and Conquer, git ignore

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static int[][] map;
-    static boolean[][] visited;
-    static int complexity;
-    static int count = 0;
-    static List<Integer> list = new ArrayList<>();
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        visited = new boolean[N][N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
-            String[] str = br.readLine().split("");
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(str[j]);
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (visited[i][j]) continue;
-                else if (!visited[i][j] && map[i][j] == 1) {
-                    count = 0;
-                    bfs(i, j);
-                    complexity++;
-                    list.add(count);
-                }
-            }
-        }
-        System.out.println(complexity);
-        list.sort(Comparator.naturalOrder());
-        for (int num : list) {
-            System.out.println(num);
-        }
+        System.out.println(solve(N, r, c));
 
     }
 
-    static void bfs(int i, int j) {
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{i, j});
-
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int x = cur[0], y = cur[1];
-            visited[x][y] = true;
-            count++;
-
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d], ny = y + dy[d];
-                if(nx<0 || nx>=N || ny<0 || ny>=N) continue;
-                if(visited[nx][ny])continue;
-                if (!visited[nx][ny] && map[nx][ny] == 1) {
-                    visited[nx][ny] = true;
-                    q.offer(new int[]{nx, ny});
-                }
-            }
-
-        }
+    private static long solve(int n, int r, int c) {
+        if(n==0) return 0;
+        int half = 1 << (n - 1); //2^n-1;
+        int quadrant = (r >= half ? 2 : 0) + (c >= half ? 1 : 0);
+        long base = (long)quadrant * half * half; //이전 사분면까지 개수.
+        return base + solve(n - 1, r % half, c % half); //사분면 축소, 행,열도 로컬좌표로 축소
 
     }
-
 }
