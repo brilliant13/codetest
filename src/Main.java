75b4f5cf2c77fb4c2e81e52f0866c9a16b968e41
Woong Jung <kaka366@naver.com>
2025-10-01 12:26:34 +0900

    backTraking Diagonal

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-    static int N;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static int[][] map;
-    static boolean[][] visited;
-    static int complexity;
-    static int count = 0;
-    static List<Integer> list = new ArrayList<>();
-
+    static int N, count;
+    static boolean[] col; //열 사용 여부
+    static boolean[] d1; // diagonal1. ↙↗ 대각선 (r+c)
+    static boolean[] d2; // diagonal2. ↘↖ 대각선 (r-c+N-1)
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        visited = new boolean[N][N];
-
-        for (int i = 0; i < N; i++) {
-            String[] str = br.readLine().split("");
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(str[j]);
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (visited[i][j]) continue;
-                else if (!visited[i][j] && map[i][j] == 1) {
-                    count = 0;
-                    bfs(i, j);
-                    complexity++;
-                    list.add(count);
-                }
-            }
-        }
-        System.out.println(complexity);
-        list.sort(Comparator.naturalOrder());
-        for (int num : list) {
-            System.out.println(num);
-        }
-
+        col = new boolean[N];
+        d1 = new boolean[2 * N - 1];
+        d2 = new boolean[2 * N - 1];
+        dfs(0);
+        System.out.print(count);
     }
 
-    static void bfs(int i, int j) {
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{i, j});
-
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int x = cur[0], y = cur[1];
-            visited[x][y] = true;
+    static void dfs(int r) {
+        if (r == N) {
             count++;
-
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d], ny = y + dy[d];
-                if(nx<0 || nx>=N || ny<0 || ny>=N) continue;
-                if(visited[nx][ny])continue;
-                if (!visited[nx][ny] && map[nx][ny] == 1) {
-                    visited[nx][ny] = true;
-                    q.offer(new int[]{nx, ny});
-                }
-            }
-
+            return;
+        }
+        for (int c = 0; c < N; c++) {
+            int a = r + c, b = r - c + (N - 1);
+            if (col[c] || d1[a] || d2[b]) continue;
+            col[c] = d1[a] = d2[b] = true;
+            dfs(r + 1);
+            col[c] = d1[a] = d2[b] = false;
         }
 
     }
+
 
 }
