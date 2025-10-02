7dcab37debbb434ed3553cfa0f08a766c5780866
Woong Jung <kaka366@naver.com>
2025-10-02 12:31:19 +0900

    LIS : dp

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    static int N;
-    static long count;
-    static int MASK; //하위 N비트 1
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        MASK = (1<<N) -1; //하위 N비트가 전부 1
-        dfs(0,0,0,0); //cols, diag1, diag2, row
-        System.out.print(count);
+        int N = Integer.parseInt(br.readLine());
+        // 10,20,10,30,20,50
+        //String[] -> Stream<String> -> IntStream or Stream<Integer> -> int[]
+        int[] a = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int[] dp = new int[N]; //dp[i]: a[i]로 끝나는 LIS길이. 최장증가부분수열
+        Arrays.fill(dp, 1);
+
+        int ans = 1;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if(a[j] < a[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            ans = Math.max(ans,dp[i]);
+        }
+        System.out.println(ans);
     }
 
-    // cols: 사용 중인 열, diag1: ↙↗ 대각(다음행에서 <<1), diag2: ↘↖ 대각(다음행에서 >>1)
-    static void dfs(int cols, int diag1, int diag2, int row) {
-        if (row == N) { //N행 모두 배치 완료
-            count++;
-            return;
-        }
 
-        int available = ~(cols | diag1 | diag2) & MASK; //이번 행에서 가능한 자리들
-
-        while (available != 0) {
-            int p = available & -available; //최하위 1비트만 선택
-            available -= p; //해당 자리 제거
-
-            dfs(cols | p, (diag1 | p) << 1 & MASK, (diag2 | p) >> 1, row + 1);
-            // & MASK: 왼쪽 시프트 후 상위 비트 잘라내기 (안전)
-        }
-    }
 }
